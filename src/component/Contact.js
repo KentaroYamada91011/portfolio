e54db7218383e60d8a70580e0ab737b8fd9da926
Kentaro91011 <kentaro.10.11@icloud.com>
2019-06-08 11:11:34 +0900

    props修正

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -11,7 +11,7 @@ class About extends React.Component {
   render(){
     return(
       <div  className="wrapper">
-        <BorderYellow  display="true"/>
+        <BorderYellow  />
         <div className="about__main">
           <h1 className="about__title">FOLLOW ME</h1>
           <div className="about__container">
