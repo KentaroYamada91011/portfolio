70d484b5c6a61a16b4a74c6921c19bc209bbb78b
山田健太郎 <kentaro91011yamada@gmail.com>
2020-01-07 17:30:53 +0900

    fix: 文言修正

--- src/component/Work.js
+++ src/component/Work.js
@@ -20,7 +20,7 @@ class About extends React.Component {
             <div className="about__container__head skill">WORKS</div>
             <div className="cards">
               <Card img={portfolio} title="portfolioサイト" lead="Reactを使い、こちらのポートフォリオサイトを作成"/>
-              <Card img={bpp} title="コーポレートサイト" lead="Vueを使い、コーポレートサイトを作成"/>
+              <Card img={bpp} title="コーポレートサイト" lead="Vueを使い、株式会社best path partners のコーポレートサイトを作成"/>
               <Card img={fukuroo} title="フクロー" lead="Rails のsolidus を用いてECサイトを作成2020/2にリリース予定"/>
               <Card img={megurume} title="Megurume" lead="Nuxt.jsとWordpress Rest Apiを使い、github上にある'nuepress'を参照に作成"/>
               <Card img={lively} title="lively" lead="ruby on railsを使い、プログラミングをして初めて作ったWebサイト。フォロー機能、カレンダー機能などを搭載"/>
